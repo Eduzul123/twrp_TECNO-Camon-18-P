06a3cd3aad2b77447f04fcf2138d1ccf10a53ef8
Eduzul123 <eduardopuentes630@gmail.com>
2024-12-19 20:25:37 -0400

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,7 +18,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export TW_DEFAULT_LANGUAGE="es-ES"
     export OF_DEVICE_ALT="Camon18P"
 	export LC_ALL="C"
-	export FOX_AB_DEVICE=1
+	export FOX_VIRTUAL_AB_DEVICE=1
 	export FOX_EXTREME_SIZE_REDUCTION=1
 	export ALLOW_MISSING_DEPENDENCIES=true
 	export OF_QUICK_BACKUP_LIST="/data;"
