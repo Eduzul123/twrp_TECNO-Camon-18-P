4934ff7d3d40c5f4d43f7724cd5eb0a995e8339b
Eduzul123 <eduardopuentes630@gmail.com>
2025-02-09 02:52:49 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,6 +18,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export OF_DEVICE_ALT="TECNO CH7n"
 	export LC_ALL="C"
 	export FOX_VIRTUAL_AB_DEVICE=1
+ 	export FOX_EXTREME_SIZE_REDUCTION=1
 	export ALLOW_MISSING_DEPENDENCIES=true
 	export OF_QUICK_BACKUP_LIST="/data;"
 	export OF_USE_MAGISKBOOT=1
@@ -26,7 +27,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 	export FOX_REMOVE_BASH=1
-        export FOX_REMOVE_AAPT=1
+	export FOX_REMOVE_AAPT=1
 	export FOX_DELETE_MAGISK_ADDON=1
         export FOX_DELETE_INITD_ADDON=1
         export FOX_EXCLUDE_NANO_EDITOR=1
@@ -74,7 +75,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
   	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-           export FOX_EXTREME_SIZE_REDUCTION=1
  	fi
 
 fi
